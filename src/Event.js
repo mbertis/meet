a8947f81c149d5110a3cf5f32a887adfa1163b11
mbertis <madisonbertis@gmail.com>
2021-02-16 14:55:30 -0800

    added tests and code for Feature 2 - show/hide event details

--- src/Event.js
+++ src/Event.js
@@ -1,8 +1,51 @@
 import React, { Component } from "react";
 
 class Event extends Component {
+  state = {
+    isExpanded: false, //Event details are collapsed by default
+  };
+
+  //Toggles show/hide details
+  handleButtonClick = () => {
+    if (this.state.isExpanded === false) {
+      this.setState({
+        isExpanded: true,
+      });
+    } else
+      this.setState({
+        isExpanded: false,
+      });
+  };
+
+  showExpandedDetails = () => {
+    const { event } = this.props;
+    if (this.state.isExpanded === true) {
+      return (
+        <div className="expanded-event-details">
+          <h1>About This Event:</h1>
+          <a className="event-link" href={event.htmlLink}>See Details on Google Calendar</a>
+          <p className="description">{event.description}</p>
+        </div>
+      );
+    }
+  };
+
   render() {
-    return <div></div>;
+    const { event } = this.props;
+    return (
+      <div className="event">
+        <h1 className="summary">{event.summary}</h1>
+        <div>
+          <p className="startDate">{event.start.dateTime}</p>
+          <p className="timeZone">{event.start.timeZone}</p>
+          <h3 className="location">{event.location}</h3>
+          <div>{this.showExpandedDetails()}</div>
+          <button className="showDetails" onClick={this.handleButtonClick}>
+            Show Details
+          </button>
+        </div>
+      </div>
+    );
   }
 }
 
---
+++ src/__tests__/Event.test.js
@@ -0,0 +1,44 @@
+import React from "react";
+import { shallow } from "enzyme";
+import Event from "../Event";
+import { mockData } from "../mock-data";
+
+describe("<Event /> component", () => {
+  let event, EventWrapper;
+  beforeAll(() => {
+    event = mockData[0];
+    EventWrapper = shallow(<Event event={event} />);
+  });
+  test("event details should be collapsed by default", () => {
+    expect(EventWrapper.state("isExpanded")).toBe(false);
+  });
+  test("should render event summary when collapsed", () => {
+    expect(EventWrapper.find(".summary").text()).toBe(event.summary);
+  });
+  test("should render date when collapsed", () => {
+    expect(EventWrapper.find(".startDate").text()).toBe(event.start.dateTime);
+  });
+  test("should render event time zone when collapsed", () => {
+    expect(EventWrapper.find(".timeZone").text()).toBe(event.start.timeZone);
+  });
+  test("should render event location when collapsed", () => {
+    expect(EventWrapper.find(".location").text()).toBe(event.location);
+  });
+  test("should render show details button", () => {
+    expect(EventWrapper.find(".showDetails")).toHaveLength(1);
+  });
+  test("show details button should expand event when clicked", () => {
+    EventWrapper.find(".showDetails").simulate("click");
+    expect(EventWrapper.state("isExpanded")).toBe(true);
+  });
+  test("should render event link when expanded", () => {
+    expect(EventWrapper.find(".event-link").props().href).toBe(event.htmlLink);
+  });
+  test("should render event description when expanded", () => {
+    expect(EventWrapper.find(".description").text()).toBe(event.description);
+  });
+  test("event details should be collapsed when button is clicked again", () => {
+    EventWrapper.find(".showDetails").simulate("click");
+    expect(EventWrapper.state("isExpanded")).toBe(false);
+  });
+});
