1b29b5c17490cd4159104fe9d0756e5578366b84
mbertis <madisonbertis@gmail.com>
2021-02-26 15:14:23 -0800

    added test and code for getting a list of all events when user selects 'see all cities'

--- src/CitySearch.js
+++ src/CitySearch.js
@@ -42,7 +42,7 @@ class CitySearch extends Component {
               {suggestion}
             </li>
           ))}
-          <li key="all">
+          <li key="all" onClick={() => this.handleItemClicked("all")}>
             <b>See all cities</b>
           </li>
         </ul>
