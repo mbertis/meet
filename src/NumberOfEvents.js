46ba43430ad50c03631c869fd5853deea307311b
mbertis <madisonbertis@gmail.com>
2021-03-02 21:14:44 -0800

    passed in both parameters for updateEvents

--- src/NumberOfEvents.js
+++ src/NumberOfEvents.js
@@ -10,7 +10,7 @@ class NumberOfEvents extends Component {
     this.setState({
       numberOfEvents: value,
     });
-    this.props.updateEvents(value);
+    this.props.updateEvents("all", value);
   };
 
   render() {
