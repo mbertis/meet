b225a2b92164be783ab161abae0190cb0aebe9bd
mbertis <madisonbertis@gmail.com>
2021-03-05 12:49:18 -0800

    installed dependencies for Puppeteer on Ubuntu and modified puppeteer.launch

--- src/__tests__/EndToEnd.test.js
+++ src/__tests__/EndToEnd.test.js
@@ -1,19 +1,27 @@
-import { puppeteer } from "puppeteer";
+import puppeteer from "puppeteer";
 
-describe("show/hide an event details", () => {
-  test("An event element is collapsed by default", async () => {
-    const browser = await puppeteer.launch({
+describe("show/hide an event details", () => {  
+  let browser;
+  let page;
+  beforeAll(async () => {
+    jest.setTimeout(30000);
+    browser = await puppeteer.launch({
+      headless: false,
       ignoreDefaultArgs: ['--disable-extensions'],
-      executablePath: "/usr/bin/chromium-browser"
-    });
-
-    const page = await browser.newPage();
-    await page.goto("https://localhost:3000/");
-
+      args: ['--no-sandbox', '--disable-setuid-sandbox']
+      // executablePath: "/usr/bin/chromium-browser"
+    });   
+    page = await browser.newPage(); 
+    await page.goto("http://localhost:3000/");
     await page.waitForSelector(".event"); // wait for the Event component to be loaded, otherwise there will be nothing to test
+  });
 
+  afterAll(() => {
+    browser.close();
+  });
+
+  test("An event element is collapsed by default", async () => {
     const eventDetails = await page.$(".event .event__Details");
     expect(eventDetails).toBeNull();
-    browser.close();
   });
 });
