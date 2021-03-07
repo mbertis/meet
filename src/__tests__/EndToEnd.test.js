4abc88c3b029f719322c3da5fa8aceb7a16c296f
mbertis <madisonbertis@gmail.com>
2021-03-07 14:31:29 -0800

    completed acceptance testing for first feature

--- src/__tests__/EndToEnd.test.js
+++ src/__tests__/EndToEnd.test.js
@@ -24,4 +24,16 @@ describe("show/hide an event details", () => {
     const eventDetails = await page.$(".event .event__Details");
     expect(eventDetails).toBeNull();
   });
+
+  test("User can expand an event to see its details", async () => {
+    await page.click(".event .details-btn");
+    const eventDetails = await page.$(".event .event__Details");
+    expect(eventDetails).toBeDefined();
+  });
+
+  test("User can collapse an event to hide its details", async () => {
+    await page.click(".event .details-btn");
+    const eventDetails = await page.$(".event .event__Details");
+    expect(eventDetails).toBeNull();
+  });
 });
